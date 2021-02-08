2a0f858790550b91abe242fc4358963308e8c94b
Joseph Price <pricejosephd@gmail.com>
2021-02-07 20:16:17 -0500

    Initial Commit

---
+++ scratchpad/scratchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  scratchpadApp.swift
+//  scratchpad
+//
+//  Created by Joseph Price on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct scratchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
